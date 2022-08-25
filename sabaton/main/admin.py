1f98d897205db28c9e7e3e72a49c69dba92f3cb3
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2022-08-25 10:17:51 +0300

    Delete sabaton directory

--- sabaton/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.Musician)
-admin.site.register(models.Track)
-admin.site.register(models.Album)
-admin.site.register(models.Photo)
-# Register your models here.
