240c1db6aab65f9fa5e86b8fbff3c41ff42c5a42
Peter1811 <78861235+Peter1811@users.noreply.github.com>
2022-09-15 11:37:33 +0300

    removed unnecessary sections

--- main/urls.py
+++ main/urls.py
@@ -2,17 +2,13 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from sabaton import settings
-from .views import main_page, about, participants, albums, songs, gallery, developers, album, participant
+from .views import main_page,participants, albums, gallery, album, participant
 
 urlpatterns = [
     path('', main_page, name='main_page'),
-    path('about/', about, name='about'),
     path('participants/', participants, name='participants'),
     path('participants/<int:pk>/', participant, name='participant'),
     path('albums/', albums, name='albums'),
     path('albums/<int:pk>/', album, name='album'),
-    path('tracks/', songs, name='songs'),
-    # path('tracks/<int:pk>', track, name='track'),
     path('gallery/', gallery, name='gallery'),
-    path('developers/', developers, name='developers'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -2,18 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import Participant, Album, Track
 
-menu = [{'title': 'Основная информация',
-         'url_name': "about"},
-        {'title': 'Участники группы',
+menu = [{'title': 'Участники группы',
          'url_name': "participants"},
         {'title': 'Альбомы',
          'url_name': "albums"},
-        {'title': 'Треки',
-         'url_name': "songs"},
         {'title': 'Галерея',
          'url_name': "gallery"},
-        {'title': 'Разработчики сайта',
-         'url_name': "developers"}
         ]
 
 
@@ -23,32 +17,33 @@ def main_page(request):
     return render(request, 'main\index.html', {'menu': menu, 'title': 'Главная страница'})
 
 
-def about(request):
-    return render(request, 'main\\about.html', {'menu': menu, 'title': 'О сайте', 'header': 'Страница about'})
-
 def participants(request):
     all_participants = Participant.objects.all()
-    return render(request, 'main\\participants.html', {'menu': menu, 'title': 'Участники', 'header': 'Страница participants', 'participants': all_participants})
+    return render(request, 'main\\participants.html',
+                  {'menu': menu, 'title': 'Участники', 'header': 'Страница participants',
+                   'participants': all_participants})
+
 
 def participant(request, pk):
     current_participant = Participant.objects.get(pk=pk)
-    return render(request, 'main\\participant.html', {'menu': menu, 'title': 'Участники', 'header': 'Страница participants', 'participant': current_participant})
+    return render(request, 'main\\participant.html',
+                  {'menu': menu, 'title': 'Участники', 'header': 'Страница participants',
+                   'participant': current_participant})
+
 
 def albums(request):
     all_albums = Album.objects.all().order_by("release_date")
-    return render(request, 'main\\albums.html', {'menu': menu, 'title': 'Альбомы', 'header': 'Страница albums', 'albums': all_albums})
+    return render(request, 'main\\albums.html',
+                  {'menu': menu, 'title': 'Альбомы', 'header': 'Страница albums', 'albums': all_albums})
+
 
 def album(request, pk):
     current_album = Album.objects.get(pk=pk)
     tracks = Track.objects.filter(album_id=pk)
-    return render(request, 'main\\album.html', {'menu': menu, 'title': current_album.title, 'header': 'Страница albums', 'tracks': tracks, 'album': current_album})
+    return render(request, 'main\\album.html',
+                  {'menu': menu, 'title': current_album.title, 'header': 'Страница albums', 'tracks': tracks,
+                   'album': current_album})
 
-def songs(request):
-    return render(request, 'main\\temp.html', {'menu': menu, 'title': 'Треки', 'header': 'Страница songs'})
 
 def gallery(request):
     return render(request, 'main\\temp.html', {'menu': menu, 'title': 'Галерея', 'header': 'Страница gallery'})
-
-def developers(request):
-    return render(request, 'main\\temp.html', {'menu': menu, 'title': 'Разработчики', 'header': 'Страница developers'})
-
