5eec4323a595c789b9bb9112920de17377bdc2cb
silverfatt <silvestr.fat@mail.ru>
2022-09-15 20:31:41 +0300

    pre-js

--- main/static/js/scripts.js
+++ main/static/js/scripts.js
@@ -1,3 +1,56 @@
+// var players = document.getElementsByClassName("audio-player");
+// let progress = document.getElementsByClassName("progress");
+// let playbtns = document.getElementsByClassName("playbtn");
+//
+//
+//
+// var playpause = function () {
+//   console.log(1)
+//   if (player.paused) {
+//     player.play();
+//   } else {
+//     player.pause();
+//   }
+// }
+//
+// playbtns.forEach((playbtn)=>{
+//   playbtn.addEventListener("click", playpause);
+// })
+//
+// players.forEach((player)=>{
+// player.onplay = function () {
+//   playbtn = player.querySelector(".fas")
+//   playbtn.classList.remove("fa-play");
+//   playbtn.classList.add("fa-pause");
+// }})
+//
+// players.forEach((player)=>{
+// player.onpause = function () {
+//   playbtn = player.querySelector(".fas")
+//   playbtn.classList.add("fa-play");
+//   playbtn.classList.remove("fa-pause");
+// }})
+//
+// players.forEach((player)=>{
+// player.ontimeupdate = function () {
+//   let ct = player.currentTime;
+//   current.innerHTML = timeFormat(ct);
+//   //progress
+//   let duration = player.duration;
+//   prog = Math.floor((ct * 100) / duration);
+//   progress.style.setProperty("--progress", prog + "%");
+// }})
+//
+// function timeFormat(ct) {
+//   minutes = Math.floor(ct / 60);
+//   seconds = Math.floor(ct % 60);
+//
+//   if (seconds < 10) {
+//     seconds = "0"+seconds;
+//   }
+//
+//   return minutes + ":" + seconds;
+// }
 var player = document.getElementById("player");
 let progress = document.getElementById("progress");
 let playbtn = document.getElementById("playbtn");
